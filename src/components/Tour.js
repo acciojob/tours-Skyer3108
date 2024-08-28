5926074cb2e9a6dc2645b8f3917659bc79f15fd2
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-28 18:19:00 +0530

    submitting code-Wed Aug 28 2024 18:18:57 GMT+0530 (India Standard Time)

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -6,19 +6,19 @@ const Tour=({tour,removeTour})=>{
 const {ids,name,info,image,price}=tour
     const [readMore,setReadMore]=useState(false)
     return (
-        <article id={`tour-item-${id}`}>
+        <article id={`tour-item-${ids}`}>
             <img src={image} alt={name}/>
             <div>
                 <h4>{name}</h4>
                 <h4>${price}</h4>
             </div>
 
-            <p id={`tour-item-para-${id}`}>{readMore?info:`${info.substring(0,200)}...`}
+            <p id={`tour-item-para-${ids}`}>{readMore?info:`${info.substring(0,200)}...`}
                 <button onClick={()=>setReadMore(!readMore)}>{readMore?'Show Less':'ShowMore'}</button>
                 </p>
 
 
-                <button id={`delete-btn-${id}`} onClick={()=>removeTour(ids)}>Not Intersted</button>
+                <button id={`delete-btn-${ids}`} onClick={()=>removeTour(ids)}>Not Intersted</button>
         </article>
 
     )
