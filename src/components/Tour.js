a9c695baeac8216caab4481d3913641b6af2ffd3
Skyer3108 <136808389+Skyer3108@users.noreply.github.com>
2024-08-28 18:40:04 +0530

    submitting code-Wed Aug 28 2024 18:39:59 GMT+0530 (India Standard Time)

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -3,22 +3,23 @@ import React from "react"
 import { useState } from "react"
 
 const Tour=({tour,removeTour})=>{
-const {ids,name,info,image,price}=tour
+const {id,name,info,image,price}=tour
+console.log(id)
     const [readMore,setReadMore]=useState(false)
     return (
-        <article id={`tour-item-${ids}`}>
+        <article id={`tour-item-${id}`}>
             <img src={image} alt={name}/>
             <div>
                 <h4>{name}</h4>
                 <h4>${price}</h4>
             </div>
 
-            <p id={`tour-item-para-${ids}`}>{readMore?info:`${info.substring(0,200)}...`}
+            <p id={`tour-item-para-${id}`}>{readMore?info:`${info.substring(0,200)}...`}
                 <button onClick={()=>setReadMore(!readMore)}>{readMore?'Show Less':'ShowMore'}</button>
                 </p>
 
 
-                <button id={`delete-btn-${ids}`} onClick={()=>removeTour(ids)}>Not Intersted</button>
+                <button id={`delete-btn-${id}`} onClick={()=>removeTour(id)}>Not Intersted</button>
         </article>
 
     )
